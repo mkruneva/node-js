0ad01e6a08beb96dbb877cd7c1e9b7ef0786040e
Marta Kruneva <marta.kruneva@gmail.com>
2019-02-22 14:19:55 +0000

    more commands

--- index.ts
+++ index.ts
@@ -1,3 +1,20 @@
 const rect = {
-    perimeter: (x: number, y: number) => (2 * (x + y)),
+  perimeter: (x: number, y: number) => (2 * (x + y)),
+  area: (x: number, y: number) => (x*y)
 }
+
+function solveRect (l: number, b: number){
+  console.log('Solving Reactangle with l and b ', l, b);
+
+  if (l <= 0 || b <= 0) {
+    console.log('reactangle dimentions should be greater than 0');
+  } else {
+    console.log('the area of rectange is ', rect.area(l, b));
+    console.log('the perimeter of rectange is ', rect.perimeter(l, b));
+  }
+}
+
+solveRect(2, 4);
+solveRect(3, 5);
+solveRect(0, 5);
+solveRect(-3, 4);
