bc0cc7326a4ced3484e9c726c7398ec6c331be63
mkruneva <marta.kruneva@gmail.com>
2019-02-28 10:13:13 +0100

    express static files

---
+++ node-express/index.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const http = require('http');
+const morgan = require('morgan')
+
+const hostname = 'localhost';
+const port = 3000;
+
+const app = express();
+app.use(morgan('dev'));
+
+app.use(express.static(`${__dirname}/public`));
+
+app.use((req, res, next) => {
+    res.setHeader('Content-Type', 'text/html');
+    res.end('<html><body<<h1>Express Server</h1></body</html>')
+});
+
+const server = http.createServer(app);
+server.listen(port, hostname, () => {
+    console.log(`server running at http://${hostname}:${port}`);
+})
