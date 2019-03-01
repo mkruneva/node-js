72fb95dfb39b2ae6781bc9cdf5afd23fd06d47c6
Marta Kruneva <marta.kruneva@gmail.com>
2019-03-01 10:13:13 +0100

    body-parser i

--- node-express/index.js
+++ node-express/index.js
@@ -1,13 +1,14 @@
 const express = require('express');
 const http = require('http');
-const morgan = require('morgan')
+const morgan = require('morgan');
+const bodyParser = require('body-parser');
 
 const hostname = 'localhost';
 const port = 3000;
 
 const app = express();
-
 app.use(morgan('dev'));
+app.use(bodyParser.json());
 app.use(express.static(`${__dirname}/public`));
 
 app.use((req, res, next) => {
