1b6f39224a0c670d5074ff48af5c986424167c65
Marta Kruneva <marta.kruneva@gmail.com>
2019-03-04 10:13:13 +0100

    /dishes endpoint init

--- node-express/index.js
+++ node-express/index.js
@@ -9,17 +9,39 @@ const port = 3000;
 const app = express();
 app.use(morgan('dev'));
 app.use(bodyParser.json());
+
+app.all('/dishes', (req, res, next) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  next();  // continue to look for more specs
+});
+
+app.get('/dishes', (req, res) => {
+  res.end('will send all dishes');
+});
+
+app.post('/dishes', (req, res) => {
+  // body is parsed with body-parser
+  res.end(`will add the dish ${req.body.name} with details ${req.body.description}`);
+});
+
+app.put('/dishes', (req, res) => {
+  res.statusCode = 403; // 403 not supported
+  res.end('PUT operation not supported on dishes');
+});
+
+// to be restricted later
+app.delete('/dishes', (req, res) => {
+  res.end('DELETING all dishes');
+});
+
 app.use(express.static(`${__dirname}/public`));
-
-app.use((req, res, next) => {
-
-
 app.use((req, res, next) => {
     res.setHeader('Content-Type', 'text/html');
-    res.end('<html><body<<h1>Express Server</h1></body</html>')
+    res.end('<html><body<<h1>Express Server</h1></body</html>');
 });
 
 const server = http.createServer(app);
 server.listen(port, hostname, () => {
     console.log(`server running at http://${hostname}:${port}`);
-})
+});
