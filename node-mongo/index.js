cbbe4ad2cdb89f9b8e1fbfcd7e8bc1220c6faa89
Marta Kruneva <marta.kruneva@gmail.com>
2019-03-25 10:13:13 +0100

    callbacks to promises

--- node-mongo/index.js
+++ node-mongo/index.js
@@ -5,8 +5,8 @@ const dboper = require('./operations');
 const url = 'mongodb://localhost:27017/';
 const dbname = 'confusion';
 
-MongoClient.connect(url, (err, client) => {
-  assert.equal(err, null); // assert if err is equal to null, e.g. check
+MongoClient.connect(url)
+.then(client => {
   console.log('Connected to server');
 
   const db = client.db(dbname);
@@ -14,29 +14,30 @@ MongoClient.connect(url, (err, client) => {
     "name": "New Dish",
     "description": "Initial test"
   }
-  dboper.insertDocument(db, docToInsert, 'dishes', res => {
-    console.log(`Insert document:\n ${JSON.stringify(res.ops)}`);
-
-    dboper.findDocuments(db, 'dishes', res => {
+  dboper.insertDocument(db, docToInsert, 'dishes')
+    .then(res => {
+      console.log(`Insert document:\n ${JSON.stringify(res.ops)}`);
+      return dboper.findDocuments(db, 'dishes');
+    })
+    .then(res => {
       console.log(`Documents found:\n ${JSON.stringify(res)}`);
-
-      dboper.updateDocument(db, {"name": "New Dish"}, {"description": "Updated test"}, 'dishes', res => {
-        console.log(`Document updated:\n ${JSON.stringify(res)}`);
-
-        dboper.removeDocument(db, {"name": "New Dish"}, 'dishes', res => {
-          console.log(JSON.stringify(res));
-
-             dboper.findDocuments(db, 'dishes', res => {
-                console.log(`Documents found after all:\n ${JSON.stringify(res)}`);
-
-                db.dropCollection('dishes', res => {
-                  console.log(JSON.stringify(res));
-
-                  client.close();
-                })
-             })
-        })
-      })
-    });
-  });
+      return dboper.updateDocument(db, {"name": "New Dish"}, {"description": "Updated test"}, 'dishes');
+    })
+    .then(res => {
+      console.log(`Document updated:\n ${JSON.stringify(res)}`);
+      return dboper.removeDocument(db, {"name": "New Dish"}, 'dishes');
+    })
+    .then(res => {
+      console.log(JSON.stringify(res));
+      return dboper.findDocuments(db, 'dishes');
+    })
+    .then(res => {
+      console.log(`Documents found after all:\n ${JSON.stringify(res)}`);
+      return db.dropCollection('dishes');
+    })
+    .then(res => {
+      console.log(JSON.stringify(res));
+      client.close();
+    })
 })
+.catch(err => console.log('err ', err));
--- node-mongo/operations.js
+++ node-mongo/operations.js
@@ -1,36 +1,19 @@
-const assert = require('assert');
-
-exports.insertDocument = (db, doc, collection, callback) => {
+exports.insertDocument = (db, doc, collection) => {
   const coll = db.collection(collection);
-  coll.insertOne(doc, (err, res) => {
-    assert.equal(err, null);
-    console.log(`Inserted result :${JSON.stringify(res.result.n)} documents in the ${JSON.stringify(collection)}`);
-    callback(res);
-  })
+  return coll.insertOne(doc);
 }
 
-exports.findDocuments = (db, collection, callback) => {
+exports.findDocuments = (db, collection) => {
   const coll = db.collection(collection);
-  coll.find({}).toArray((err, docs) => {
-    assert.equal(err, null);
-    callback(docs);
-  })
+  return coll.find({}).toArray();
 }
 
 exports.removeDocument = (db, doc, collection, callback) => {
   const coll = db.collection(collection);
-  coll.deleteOne(doc, (err, res) => {
-    assert.equal(err, null);
-    console.log(`Removed the document ${JSON.stringify(doc)}`);
-    callback(res);
-  })
+  return coll.deleteOne(doc)
 }
 
 exports.updateDocument = (db, doc, update, collection, callback) => {
   const coll = db.collection(collection);
-  coll.updateOne(doc, { $set: update}, null, (err, res) => {
-    assert.equal(err, null);
-    console.log(`Updated the document ${JSON.stringify(doc)} with ${JSON.stringify(update)}`);
-    callback(res);
-  })
+  return coll.updateOne(doc, { $set: update}, null);
 }
